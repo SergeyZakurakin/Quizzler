9abd821414cb379995dafa189b16380ceb48b875
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2023-07-22 17:53:24 +0100

    first version

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,28 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Sergey Zakurakin on 22/07/2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
+//    let quiz = [
+//        ["Four + Two is equel to Six", "True"],
+//        ["Five - is Three greater then one","True"],
+//        ["Three + Eight is greather than Ten", "True"]
+//    ]
+    
+    
+}
